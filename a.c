f58cb46e7684563c31d2d509d0dfa24455570139
eisuke_yamamoto <gaiya_333333@yahoo.co.jp>
2014-05-17 14:11:58 +0900

    修正版

--- a.c
+++ a.c
@@ -3,22 +3,23 @@
 int main()
 {
 	int i,j ;
-	printf("<!DOCTYPE html>\n");
-	printf("<meta charset=\"UTF-8\">\n");
-	printf("<title>99</title>\n");
-	printf("<hi>99</hi>\n");
-	printf("<table>\n");
+	printf("<!DOCTYPE html>\n") ;
+	printf("<meta charset=\"UTF-8\">\n") ;
+	printf("<title>99</title>\n") ;
+	printf("<hi>99</hi>\n") ;
+	printf("<table>\n") ;
 
 	for (int i = 0 ; i < 10 ; i++){ 
-		printf("<tr>\n") ;
+		printf("<tr>") ;
 		printf("<th>%2d",i) ;
-			for (int j = 1 ; j < 10 ; j++){ 
-				if(i == 0){
-					printf("<th>%2d",j) ;
-				}else{
-					printf("<th>%2d",i * j) ;
-				}
+		for (int j = 1 ; j < 10 ; j++){ 
+			if(i == 0){
+				printf("<th>%2d",j) ;
+			}else{
+				printf("<th>%2d",i * j) ;
 			}
+		}
+		printf("\n") ;
 	}	
 	return 0 ;
 }

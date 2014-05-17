aeabf0d736570e469d8e4b209b1d078f4d413ce2
eisuke_yamamoto <gaiya_333333@yahoo.co.jp>
2014-05-17 12:10:43 +0900

    修正後

--- a.c
+++ a.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 
-int main ()
+int main()
 {
- int i,j ;
- printf("<!DOCTYPE html>\n");
- printf("<meta charset=\"UTF-8\">\n");
- printf("<title>99</title>\n");
- printf("<hi>99</hi>\n");
- printf("<table>\n");
- 
-	for (int i = 0;i<10;i++){ 
-	
-	 printf("<tr>"\n);
-   
-	 for (int j = 0;j<10;j++){ 
-     
-	   if(i==0){
-		printf("<th>");
-		
-	   }else{
-	
-		printf("<th>%d",j)
-	   }
- }	
- return 0 ;
+	int i,j ;
+	printf("<!DOCTYPE html>\n");
+	printf("<meta charset=\"UTF-8\">\n");
+	printf("<title>99</title>\n");
+	printf("<hi>99</hi>\n");
+	printf("<table>\n");
+
+	for (int i = 0 ; i < 10 ; i++){ 
+		printf("<tr>\n") ;
+		printf("<th>%2d",i) ;
+			for (int j = 1 ; j < 10 ; j++){ 
+				if(i == 0){
+					printf("<th>%2d",j) ;
+				}else{
+					printf("<th>%2d",i * j) ;
+				}
+			}
+	}	
+	return 0 ;
 }
